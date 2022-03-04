bd6c47e0a7fa133d0e9c55e9592ced0e25b64322
Severi Casserly <severi@casserly.fi>
2022-03-04 19:02:06 +0200

    Post temperature to backend

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 const ruuvi = require('node-ruuvitag');
+const fetch = require('node-fetch');
 
 ruuvi.on('found', (tag) => {
   console.log(`Found RuuviTag with id: ${tag.id}`);
 
   tag.on('updated', (data) => {
     console.log(`${tag.id}: ${JSON.stringify(data)}`);
+
+    const body = { message: `temperature: ${data.temperature}` };
+
+    fetch('http://95.216.154.69:9000/api/chats', {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' },
+    });
   });
 });
