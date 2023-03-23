df2fb9a1331a257b30ed460ff9fe21b10b12316f
Niklas Halonen <niklas.halonen@protonmail.com>
2023-03-23 11:12:35 +0200

    add error handling

--- src/main.js
+++ src/main.js
@@ -1,18 +1,26 @@
 import ruuvi from 'node-ruuvitag';
-import fetch from 'node-fetch' ;
+import fetch from 'node-fetch';
 
 ruuvi.on('found', (tag) => {
-  console.log(`Found RuuviTag with id: ${tag.id}`);
+    console.log(`Found RuuviTag with id: ${tag.id}`);
 
-  tag.on('updated', async (data) => {
-    console.log(`${tag.id}: ${JSON.stringify(data)}`);
+    tag.on('updated', async (data) => {
+        console.log(`${tag.id}: ${JSON.stringify(data)}`);
 
-    const body = { message: `temperature: ${data.temperature}` };
+        const body = { message: `temperature: ${data.temperature}` };
 
-    await fetch('http://95.216.154.69:9000/api/chats', {
-      method: 'post',
-      body: JSON.stringify(body),
-      headers: { 'Content-Type': 'application/json' },
+        try {
+            const response = await fetch('http://95.216.207.125:9000/api/chats', {
+                method: 'post',
+                body: JSON.stringify(body),
+                headers: { 'Content-Type': 'application/json' },
+            });
+            if (!response.ok) {
+                const {status, statusText} = response;
+                console.error(`Fetch failed with status ${status} ${statusText}`);
+            }
+        } catch (error) {
+            console.error(error);
+        }
     });
-  });
 });
