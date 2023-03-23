0544ebe6f35c87eb2e0257739b1ebabda6e538a1
Niklas Halonen <niklas.halonen@protonmail.com>
2023-03-23 10:56:25 +0200

    update dependencies, fix code

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-const ruuvi = require('node-ruuvitag');
-const fetch = require('node-fetch');
+import ruuvi from 'node-ruuvitag';
+import fetch from 'node-fetch' ;
 
 ruuvi.on('found', (tag) => {
   console.log(`Found RuuviTag with id: ${tag.id}`);
 
-  tag.on('updated', (data) => {
+  tag.on('updated', async (data) => {
     console.log(`${tag.id}: ${JSON.stringify(data)}`);
 
     const body = { message: `temperature: ${data.temperature}` };
 
-    fetch('http://95.216.154.69:9000/api/chats', {
+    await fetch('http://95.216.154.69:9000/api/chats', {
       method: 'post',
       body: JSON.stringify(body),
       headers: { 'Content-Type': 'application/json' },
